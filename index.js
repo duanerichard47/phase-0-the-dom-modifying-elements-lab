18d989e2b5626dada0cfbc033f38001934b1a783
Duane <duanerichard47@hotmail.com>
2022-12-28 15:38:05 -0500

    commit tree 7b5ded72e5977be459e44eceba06986e940a45c9

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent ="Duane is the champion";
+//newHeader.innerHTML = "<h1>Duane is the champion</h1>";
+
+document.body.append(newHeader);
+
+
+
+
