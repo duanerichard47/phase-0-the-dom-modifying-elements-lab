026999f40198c7fe0661a3c1cdc311fbdc10e5a9
Duane <duanerichard47@hotmail.com>
2023-01-17 19:46:44 -0500

    complete phase o the dom modifying elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let elementMainRmove = document.querySelector('#main')
+elementMainRmove.remove()
+
+let newHeader = document.createElement("h1");
+let docBody = document.querySelector('body');
+docBody.appendChild(newHeader)
+newHeader.setAttribute('id','victory');
+
+newHeader.textContent = "Duane is the champion";
